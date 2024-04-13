9c48ae20631ba77172ecc387e69d81f87c65f6ff
demidzheludev <demid.zheludev@gmail.com>
2024-04-13 22:32:05 +0300

    add dag call_function_pending_update

---
+++ airflow/dags/orders_master_call_function_status_udpate.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from datetime import datetime
+
+dag = DAG(
+    dag_id='call_function_pending_update',
+    schedule_interval='0 20 * * *',  
+    start_date=datetime(2024, 4, 14),
+    catchup=False
+)
+
+call_postgres_function_refrash_status = PostgresOperator(
+    task_id='function_refrash_status',
+    postgres_conn_id='animals_postgres_connection', 
+    sql="SELECT f_orders_status_update();",
+    dag=dag
+)
+
+call_postgres_function_refrash_status
