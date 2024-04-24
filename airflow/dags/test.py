e4e5d485e3745270f28ec15a1f479183bc72192a
demidzheludev <demid.zheludev@gmail.com>
2024-04-24 18:20:11 +0300

    delete test dags

--- airflow/dags/test.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-import psycopg2  
-from master_functions import *
-import pandas as pd
-import random
-from datetime import datetime        
-      
-x = master_orders_dataset()
-print(x)
