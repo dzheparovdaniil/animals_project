763871ffcab83b937f30249e61056a5542839def
demidzheludev <demid.zheludev@gmail.com>
2024-06-05 01:55:38 +0300

    staging dag fixed

--- airflow/dags/dag_staging_transfer.py
+++ airflow/dags/dag_staging_transfer.py
@@ -4,26 +4,17 @@ from airflow.operators.python_operator import PythonOperator
 
 def staging_orders_dataset():
     engine = postgresql_engine()
-    min_date = """select coalesce(min(order_date_mo), current_date) as min_date 
-                  from(select mo.id as id_mo, 
-                  so.id as id_so, 
-                  mo.order_date as order_date_mo, 
-                  so.order_date as order_date_so,
-                  mo.status as status_mo, 
-                  so.status as status_so 
-                  from master.orders mo left join staging.orders so on mo.id = so.id) as subq
-                  where status_mo <> status_so or status_so is null"""  
-    last_val = get_one_value_from_db(engine, min_date)
+    min_id = """select min(mo.id) as min_id
+                        from master.orders mo left join staging.total_orders so on mo.id = so.id
+                        where so.id is null"""  
+    last_val = get_one_value_from_db(engine, min_id)
     actual_master_orders_query = """select mo.id, mo.order_date, mo.status, 
                                     mo.user_id, mo.source_path, crm.rent_start, crm.rent_end,
                                     crm.rent_end - crm.rent_start AS rent_days, crm.promocode
                                     from master.orders mo
                                     left join master.crm_rent crm ON mo.id = crm.id
-                                    where mo.order_date >= '{}' order by mo.id""".format(last_val)     
+                                    where mo.id >= '{}' order by mo.id""".format(last_val)     
     orders_for_staging_data = get_table_from_db(engine, actual_master_orders_query)
-    delete_sql_query = f"DELETE FROM staging.orders WHERE order_date >= '{last_val}'"
-    engine.execute(delete_sql_query)
-    print(f"Удалены строки в таблице staging.orders начиная с даты {last_val}")
     return orders_for_staging_data, engine 
 
 def staging_items_dataset():
@@ -49,7 +40,7 @@ with DAG(
           orders_staging_task = PythonOperator(
                   task_id = 'orders_staging_transfer',
                   python_callable=download_to_master, 
-                  op_kwargs={'func_dataset': staging_orders_dataset(), 'table': 'orders', 'schema': 'staging'}
+                  op_kwargs={'func_dataset': staging_orders_dataset(), 'table': 'total_orders', 'schema': 'staging'}
           )
 
           items_staging_task = PythonOperator(
