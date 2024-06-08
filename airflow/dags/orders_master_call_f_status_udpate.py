ddef702dae415dbee293d281f9f6343352614269
demidzheludev <demid.zheludev@gmail.com>
2024-06-08 14:45:48 +0300

    add update functions

---
+++ airflow/dags/orders_master_call_f_status_udpate.py
@@ -0,0 +1,37 @@
+from master_functions import *
+from datetime import datetime
+from airflow import DAG 
+from airflow.operators.python_operator import PythonOperator
+from sqlalchemy import create_engine
+
+def master_status_update():
+    db_uri = 'postgresql://demid:demid123@158.160.169.66:5432/postgres'
+
+    # объект подключения к базе данных
+    engine = create_engine(db_uri)
+
+    connection = engine.connect()
+    trans = connection.begin()
+
+    # Вызов функции staging.function_orders_status_update() и получение результата
+    result = connection.execute("SELECT master.function_orders_status_update();").fetchone()
+
+    # Вывод результата
+    print(result[0])
+
+    trans.commit()
+    connection.close()
+
+with DAG(
+          dag_id='call_function_pending_update_for_master',
+          start_date = datetime(2024, 4, 30),
+          schedule_interval='20 13 * * *',
+          catchup=False
+) as dag:
+
+          call_refresh_master_orders = PythonOperator(
+                  task_id = 'call_function_pending_update_for_master_task',
+                  python_callable=master_status_update()
+          )
+          
+call_refresh_master_orders 
---
+++ airflow/dags/staging_call_f_status_update.py
@@ -0,0 +1,37 @@
+from master_functions import *
+from datetime import datetime
+from airflow import DAG 
+from airflow.operators.python_operator import PythonOperator
+from sqlalchemy import create_engine
+
+def staging_status_update():
+    db_uri = 'postgresql://demid:demid123@158.160.169.66:5432/postgres'
+
+    # объект подключения к базе данных
+    engine = create_engine(db_uri)
+
+    connection = engine.connect()
+    trans = connection.begin()
+
+    # Вызов функции staging.function_orders_status_update() и получение результата
+    result = connection.execute("SELECT staging.function_orders_status_update();").fetchone()
+
+    # Вывод результата
+    print(result[0])
+
+    trans.commit()
+    connection.close()
+
+with DAG(
+          dag_id='call_function_pending_update_for_staging',
+          start_date = datetime(2024, 6, 7),
+          schedule_interval='20 14 * * *',
+          catchup=False
+) as dag:
+
+          call_refresh_staging_orders = PythonOperator(
+                  task_id = 'call_function_pending_update_for_staging_task',
+                  python_callable=staging_status_update
+          )
+          
+call_refresh_staging_orders    
