028a321d1d16f6b9daf31bed63380298700a125c
demidzheludev <demid.zheludev@gmail.com>
2024-06-20 02:47:38 +0300

    add dag clickhouse_dwh_costs_transfer

---
+++ airflow/dags/clickhouse_costs.py
@@ -0,0 +1,33 @@
+from master_functions import *
+from airflow import DAG 
+from airflow.operators.python_operator import PythonOperator  
+import clickhouse_connect
+
+def get_staging_costs():
+    connection = postgresql_engine()
+    data_costs = get_table_from_db(connection, "select * from staging.costs")
+    connection.close()
+    return data_costs
+
+def clickhouse_costs_transfer_function():
+    client = clickhouse_connect.get_client(host='158.160.169.66', username='', password='') 
+    costs = get_staging_costs()
+    database_name = 'my_database'
+    table_name = 'costs'
+    client.query(f'TRUNCATE TABLE {database_name}.{table_name}')
+    client.insert_df(f'{database_name}.{table_name}', costs)
+    client.close()
+
+with DAG(
+          dag_id='clickhouse_dwh_costs_transfer',
+          start_date = datetime(2024, 6, 18),
+          schedule_interval='15 15 * * *',
+          catchup=False
+) as dag:
+
+          clickhouse_dwh_transfer_costs_task = PythonOperator(
+                  task_id = 'costs_download_to_dwh',
+                  python_callable=clickhouse_costs_transfer_function                  
+          )
+          
+clickhouse_dwh_transfer_costs_task       
